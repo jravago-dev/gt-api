7f65869d9e2303d397dc1ec6ad53126851c7f4e1
Justin R <justinshobby@zoho.com>
2019-08-17 13:50:21 +0800

    Juts (0817) - Added initial rework files for gt api

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    // ENV vars here
+    serverPort: process.env.PORT,
+    dbConnection: process.env.NODE_ENV === 'development' ? process.env.MONGO_LOCAL : process.env.MONGO_PROD
+
+}
